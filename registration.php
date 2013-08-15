041f518b0131d4030d680110cd6a1ac35cfb3ff7
Itumeleng <seanypaully@gmail.com>
2013-08-15 12:23:46 -0700

    Add By Weekly

---
+++ registration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<?php
+	$text_username = $_POST["username"];
+	$text_name = $_POST["name"];
+	$text_email = $_POST["email"];
+	$text_mobileNum = $_POST["mobileNum"];
+	$text_pass = $_POST["password"];
+	$text_repass = $_POST["repass"];
+	
+?>
+</head>
+
+<body>
+</body>
+</html>
